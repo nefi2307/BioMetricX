e80c5031c80d18262ed700208ac6da37f8aa2005
nefi2307 <alu.21130937@correo.itlalaguna.edu.mx>
2024-12-08 15:08:02 -0600

    Creation GitHub repository

--- app/src/main/java/com/example/biometricx/MainActivity.kt
+++ app/src/main/java/com/example/biometricx/MainActivity.kt
@@ -9,39 +9,35 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.biometricx.ui.theme.BioMetricXTheme
+
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.biometricx.ui.theme.MainApp
+import com.example.biometricx.SignupScreen
+import com.example.biometricx.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BioMetricXTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MainApp {
+                NavigationView()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun NavigationView() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BioMetricXTheme {
-        Greeting("Android")
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "welcome") {
+        // also pass navController to each screen so we can use navController in there
+        composable("welcome") { WelcomeScreen(navController) }
+        composable("login") { LoginScreen(navController) }
+        composable("signup") { SignupScreen(navController) }
     }
 }
